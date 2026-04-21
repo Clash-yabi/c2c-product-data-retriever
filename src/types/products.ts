58d2ac6eb9f96bdb80ddb24b4f69100edf55fb6c
«Clash-yabi» <yabesolo@student.arteveldehs.be>
2026-04-21 09:12:57 +0200

    feat: add C2CProduct type definitions

---
+++ src/types/products.ts
@@ -0,0 +1,12 @@
+export interface C2CProduct {
+  company: string;
+  productName: string;
+  level: string;
+  standardVersion: string;
+  slug: string;
+  effectiveDate?: string;
+  expirationDate?: string;
+  leadAssessmentBody?: string;
+  materialHealthAssessmentBody?: string;
+  pdfUrl?: string;
+}
